29287929da4909a7accb052608056dcc5cb08862
Manojnagula <manojnagula44@gmail.com>
2023-09-04 18:56:11 +0530

    completed pagenation

---
+++ src/components/pokemon/Pokemon.jsx
@@ -0,0 +1,16 @@
+import './Pokemon.css'
+
+
+function Pokemon({name,url}){
+    console.log(url);
+    return(
+        <div className='pokemon'>
+            <div className='pokemon-name'>{name}</div>
+            <div>
+            <img className='pokemon-image' src={url}/> 
+        </div>
+        </div>
+      
+    )
+}
+export default Pokemon;
